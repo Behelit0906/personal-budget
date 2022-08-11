048523a094e37c7988f3d08c682d825f4dcc0dcb
Luis Salcedo <luis.salcedo0906@gmail.com>
2022-08-11 10:24:48 -0500

    setting attribute names for environment variables

--- api/config/config.js
+++ api/config/config.js
@@ -6,7 +6,7 @@ export const port = process.env.PORT || 3000;
 
 export default {
     host: process.env.DB_HOST || "",
-    dbName: process.env.DB_NAME || "",
-    dbUser: process.env.DB_USER || "",
-    dbPass: process.env.DB_PASS || ""
+    user: process.env.DB_USER || "",
+    password: process.env.DB_PASS || "",
+    database: process.env.DB_NAME || ""
 };
