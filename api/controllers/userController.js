5563e4d76bed0abee5ee5c11e6dd512bc8cb5c86
Luis Salcedo <luis.salcedo0906@gmail.com>
2022-08-19 11:35:35 -0500

    method for login

--- api/controllers/userController.js
+++ api/controllers/userController.js
@@ -22,5 +22,19 @@ const createNewUser = async (req, res) => {
     }
 }
 
-export default {createNewUser};
+const userAuthenticator = async (req, res) => {
+    const {email, password} = req.body;
+    
+    try{
+        const result = await userService.userAuthenticator(email, password);
+        if(!result) return res.status(401).send({message: 'Incorrect email or password'});
+        return res.send({jwt:result});
+    }
+    catch(error){
+        return res.status(500).send({message: 'Oops, something seems to be wrong, please try again later'})
+    }
+     
+};
+
+export default {createNewUser, userAuthenticator};
 
