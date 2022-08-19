1d7798fe4a015d5afd8e2a571de2ba0318de4975
Luis Salcedo <luis.salcedo0906@gmail.com>
2022-08-19 09:00:09 -0500

    set up operation routes in express app

--- api/app.js
+++ api/app.js
@@ -1,12 +1,14 @@
 import express from "express";
 import { PORT } from "./config/config.js";
 import userRouter from "./v1/routes/userRoutes.js";
+import operationRouter from "./v1/routes/operationRoutes.js";
 
 
 const app = express();
 
 app.use(express.json());
 app.use(userRouter);
+app.use('/operations',operationRouter);
 
 app.listen(PORT, () => {
     console.log(`Listening on port ${PORT}...`);
