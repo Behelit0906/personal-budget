f27406dff3fded4e877e633d647e00dd58069abe
Luis Salcedo <luis.salcedo0906@gmail.com>
2022-08-20 16:22:59 -0500

    endpoint login validations

---
+++ api/middlewares/validateLogin.js
@@ -0,0 +1,32 @@
+import loginDtoSchema from "../dto/loginDto.js";
+import Ajv from "ajv";
+import addErrors from "ajv-errors";
+import addFormat  from "ajv-formats";
+
+const ajv = new Ajv({allErrors:true});
+addFormat(ajv, ['email','regex']);
+addErrors(ajv, {keepErrors:false});
+
+const validator = ajv.compile(loginDtoSchema);
+
+const loginValidator = (req, res, next) => {
+    console.log('hola');
+    try{
+        const isDtoValid = validator(req.body);
+        
+        if(!isDtoValid){
+            let errors = validator.errors.map((e) => {
+                return e.message;
+            })
+            return res.status(400).send({message:errors});          
+        }
+
+        next();
+    }
+    catch(error){
+        console.log('hola');
+    }
+    
+}
+
+export default loginValidator; 
--- api/v1/routes/userRoutes.js
+++ api/v1/routes/userRoutes.js
@@ -1,11 +1,12 @@
 import { Router } from "express";
 import userController from "../../controllers/userController.js";
+import loginValidator from "../../middlewares/validateLogin.js";
 
 
 const userRouter = Router();
 
 userRouter.post('/register', userController.createNewUser);
-userRouter.post('/login', userController.userAuthenticator);
+userRouter.post('/login', loginValidator, userController.userAuthenticator);
 
 
 
