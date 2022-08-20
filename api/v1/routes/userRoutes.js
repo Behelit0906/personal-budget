96acffda66be0bdaf9e0ee553f5cf9544370b30d
Luis Salcedo <luis.salcedo0906@gmail.com>
2022-08-20 17:39:15 -0500

    setting middleware in user register endpoint

--- api/v1/routes/userRoutes.js
+++ api/v1/routes/userRoutes.js
@@ -1,11 +1,12 @@
 import { Router } from "express";
 import userController from "../../controllers/userController.js";
 import loginValidator from "../../middlewares/validateLogin.js";
+import RegisterValidator from "../../middlewares/validateUserRegister.js";
 
 
 const userRouter = Router();
 
-userRouter.post('/register', userController.createNewUser);
+userRouter.post('/register',RegisterValidator ,userController.createNewUser);
 userRouter.post('/login', loginValidator, userController.userAuthenticator);
 
 
