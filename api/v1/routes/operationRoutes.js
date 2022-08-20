65323f7b1920074be3706d26ed2fbe876919f1d9
Luis Salcedo <luis.salcedo0906@gmail.com>
2022-08-20 07:32:38 -0500

    assigning token validator middleware

--- api/v1/routes/operationRoutes.js
+++ api/v1/routes/operationRoutes.js
@@ -1,7 +1,9 @@
 import { Router } from "express";
 import operationController from "../../controllers/operationController.js";
+import tokenValidator from "../../middlewares/validateToken.js";
 
 const operationRouter = Router();
+operationRouter.use(tokenValidator);
 
 operationRouter.get('/',operationController.getAllOperations);
 operationRouter.get('/:operationId',operationController.getOneOperation);
