7f403e53fda5b9c4d076c2ab9eaf4615091af80a
Luis Salcedo <luis.salcedo0906@gmail.com>
2022-08-21 18:14:39 -0500

    assigning validation middlwares

--- api/v1/routes/operationRoutes.js
+++ api/v1/routes/operationRoutes.js
@@ -1,14 +1,15 @@
 import { Router } from "express";
 import operationController from "../../controllers/operationController.js";
 import tokenValidator from "../../middlewares/validateToken.js";
+import operationValidate from "../../middlewares/operationValidate.js";
 
 const operationRouter = Router();
 operationRouter.use(tokenValidator);
 
 operationRouter.get('/',operationController.getAllOperations);
 operationRouter.get('/:operationId',operationController.getOneOperation);
-operationRouter.post('/', operationController.createNewOperation);
-operationRouter.patch('/:operationId',operationController.updateOperation);
+operationRouter.post('/', operationValidate.createValidation, operationController.createNewOperation);
+operationRouter.patch('/:operationId', operationValidate.updateValidation,operationController.updateOperation);
 operationRouter.delete('/:operationId',operationController.deleteOperation);
 
 
