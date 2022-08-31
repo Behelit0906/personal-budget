8a895f25752a33b0304f26cca30826dfdd9b1811
Luis Salcedo <luis.salcedo0906@gmail.com>
2022-08-31 11:51:47 -0500

    adjust the parameters of the update query

--- api/database/operationDao.js
+++ api/database/operationDao.js
@@ -5,40 +5,40 @@ const connection = mysql.createConnection(config);
 
 const createNewOperation = async (userId, newOperation) => {
     const query = 'INSERT INTO operations (name, type, amount, date, user_id) VALUES (?,?,?,?,?)';
-    const {name, type, amount, date} = newOperation;
+    const { name, type, amount, date } = newOperation;
     const values = [name, type, amount, date, userId];
 
-    try{
+    try {
         const result = await (await connection).query(query, values);
         return result.affectedRows;
     }
-    catch(error){
+    catch (error) {
         throw error;
-    } 
+    }
 
 };
 
 const deleteOperation = async (userId, operationId) => {
     const query = 'DELETE FROM operations WHERE id = ? AND user_id = ?';
-    try{
+    try {
         const result = await (await connection).query(query, [operationId, userId]);
         return result.affectedRows;
     }
-    catch(error){
+    catch (error) {
         throw error;
     }
 };
 
 const updateOperation = async (userId, operationData) => {
-    const query = `UPDATE operations SET name = ?, amount = ?, date = ? WHERE id = ${operationData.id} and user_id=${userId}`;
-    const {name, amount, date} = operationData;
-    const values = [name, amount, date];
+    const query = 'UPDATE operations SET name = ?, amount = ?, date = ? WHERE id = ? and user_id = ?';
+    const { name, amount, date, id } = operationData;
+    const values = [name, amount, date, id, userId];
 
-    try{
+    try {
         const result = await (await connection).query(query, values);
         return result.affectedRows;
     }
-    catch(error){
+    catch (error) {
         throw error;
     }
 
@@ -46,41 +46,41 @@ const updateOperation = async (userId, operationData) => {
 
 const getOneOperation = async (userId, operationId) => {
     const query = 'SELECT id, name, type, amount, date FROM operations WHERE id = ? and user_id=?';
-    try{
+    try {
         const result = await (await connection).query(query, [operationId, userId]);
         return result;
     }
-    catch(error){
+    catch (error) {
         throw error;
     }
 }
 
 const getAllOperations = async (userId) => {
     const query = 'SELECT id, name, type, amount, date FROM operations WHERE user_id = ?';
-    try{
+    try {
         const result = await (await connection).query(query, userId);
         return result;
     }
-    catch(error){
+    catch (error) {
         throw error;
     }
 }
 
 const getANumberOfOperations = async (values) => {
     const query = 'SELECT id, name, type, amount, date FROM operations WHERE user_id = ? LIMIT ?,?';
-    try{
+    try {
         const result = await (await connection).query(query, values);
         return result;
     }
-    catch(error){
+    catch (error) {
         throw error;
     }
 };
 
 export default {
-    createNewOperation, 
-    deleteOperation, 
-    updateOperation, 
+    createNewOperation,
+    deleteOperation,
+    updateOperation,
     getOneOperation,
     getAllOperations,
     getANumberOfOperations
