211f37319cfea6d7751cf44b868c8281a2c42bb1
Luis Salcedo <luis.salcedo0906@gmail.com>
2022-08-31 12:20:31 -0500

    versioning imported routes

--- api/app.js
+++ api/app.js
@@ -1,14 +1,14 @@
 import express from "express";
 import { PORT } from "./config/config.js";
-import userRouter from "./v1/routes/userRoutes.js";
-import operationRouter from "./v1/routes/operationRoutes.js";
+import v1UserRouter from "./v1/routes/userRoutes.js";
+import v1OperationRouter from "./v1/routes/operationRoutes.js";
 
 
 const app = express();
 
 app.use(express.json());
-app.use('/api/v1/users',userRouter);
-app.use('/api/v1/operations',operationRouter);
+app.use('/api/v1/users', v1UserRouter);
+app.use('/api/v1/operations', v1OperationRouter);
 
 app.listen(PORT, () => {
     console.log(`Listening on port ${PORT}...`);
