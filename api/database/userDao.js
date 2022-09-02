a4d5b0ca2800dacb37335018c0612d2a0f6a2305
Luis Salcedo <luis.salcedo0906@gmail.com>
2022-09-02 09:18:49 -0500

    eliminating unnecessary try/catch blocks

--- api/database/userDao.js
+++ api/database/userDao.js
@@ -1,27 +1,18 @@
 import mysql from 'promise-mysql';
 import config from '../config/config.js';
 
-const connection = mysql.createConnection(config);
+const connection = await mysql.createConnection(config);
 
 const createNewUser = async newUser => {
   const query = `INSERT INTO users (name, email, password) VALUES ('${newUser.name}', '${newUser.email}', '${newUser.password}')`;
-
-  try {
-    const result = await (await connection).query(query);
-    return result.affectedRows;
-  } catch (error) {
-    throw error;
-  }
+  const result = await connection.query(query);
+  return result.affectedRows;
 };
 
 const findUserByEmail = async email => {
   const query = `SELECT * FROM users WHERE email = '${email}'`;
-  try {
-    const result = await (await connection).query(query);
-    return result;
-  } catch (error) {
-    throw error;
-  }
+  const result = await connection.query(query);
+  return result;
 };
 
 export default { createNewUser, findUserByEmail };
