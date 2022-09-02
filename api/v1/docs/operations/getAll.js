29085937ee24396360e469343dd12439b260076e
Luis Salcedo <luis.salcedo0906@gmail.com>
2022-09-02 07:12:43 -0500

    documentation scheme for the endpoint getAll

---
+++ api/v1/docs/operations/getAll.js
@@ -0,0 +1,27 @@
+/**
+ * @openapi
+ * /api/v1/operations/:
+ *   get:
+ *     tags:
+ *       - Operations
+ *     responses:
+ *       200:
+ *         description: A JSON array of operations
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 data:
+ *                   type: array 
+ *                   items: 
+ *                     type: object
+ *                     example: 
+ *                              id: 5 
+ *                              name: Blue jeans
+ *                              type: Egress
+ *                              amount: 57
+ *                              date: 2022-08-18T05:00:00.000Z
+ *                
+ *                      
+ */
