30e6ba76b8309bd63daf796e7d2323692ba32b8b
Luis Salcedo <luis.salcedo0906@gmail.com>
2022-09-06 08:44:22 -0500

    adjusting the parameters of the create user method

--- api/database/userDao.js
+++ api/database/userDao.js
@@ -4,9 +4,8 @@ import config from '../config/config.js';
 const connection = await mysql.createConnection(config);
 
 const createNewUser = async newUser => {
-  const query = `INSERT INTO users (name, email, password) VALUES ('${newUser.name}', '${newUser.email}', '${newUser.password}')`;
-  const result = await connection.query(query);
-  return result.affectedRows;
+  const query = 'INSERT INTO users (name, email, password) VALUES (?, ?, ?)';
+  await connection.query(query, newUser);
 };
 
 const findUserByEmail = async email => {
