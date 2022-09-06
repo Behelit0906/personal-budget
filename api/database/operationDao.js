ce6b9e1cf81dfb86e0ea4b5371000bbeca730589
Luis Salcedo <luis.salcedo0906@gmail.com>
2022-09-06 09:50:18 -0500

    adjusting the handling of promises in all methods

--- api/database/operationDao.js
+++ api/database/operationDao.js
@@ -1,29 +1,29 @@
 import mysql from 'promise-mysql';
 import config from '../config/config.js';
 
-const connection = mysql.createConnection(config);
+const connection = await mysql.createConnection(config);
 
 const createNewOperation = async data => {
   const query = 'INSERT INTO operations (name, type, amount, date, user_id) VALUES (?,?,?,?,?)';
-  await (await connection).query(query, data);
+  await connection.query(query, data);
 };
 
 const deleteOperation = async (userId, operationId) => {
   const query = 'DELETE FROM operations WHERE id = ? AND user_id = ?';
-  const result = await (await connection).query(query, [operationId, userId]);
+  const result = await connection.query(query, [operationId, userId]);
   return result.affectedRows;
 };
 
 const updateOperation = async data => {
   const query = 'UPDATE operations SET name = ?, amount = ?, date = ? WHERE id = ? and user_id = ?';
-  const result = await (await connection).query(query, data);
+  const result = await connection.query(query, data);
   return result.affectedRows;
 };
 
 const getOneOperation = async (userId, operationId) => {
   const query = 'SELECT id, name, type, amount, date FROM operations WHERE id = ? and user_id=?';
   try {
-    const result = await (await connection).query(query, [operationId, userId]);
+    const result = await connection.query(query, [operationId, userId]);
     return result;
   } catch (error) {
     throw error;
@@ -33,7 +33,7 @@ const getOneOperation = async (userId, operationId) => {
 const getAllOperations = async userId => {
   const query = 'SELECT id, name, type, amount, date FROM operations WHERE user_id = ?';
   try {
-    const result = await (await connection).query(query, userId);
+    const result = await connection.query(query, userId);
     return result;
   } catch (error) {
     throw error;
@@ -43,7 +43,7 @@ const getAllOperations = async userId => {
 const getANumberOfOperations = async values => {
   const query = 'SELECT id, name, type, amount, date FROM operations WHERE user_id = ? LIMIT ?,?';
   try {
-    const result = await (await connection).query(query, values);
+    const result = await connection.query(query, values);
     return result;
   } catch (error) {
     throw error;
