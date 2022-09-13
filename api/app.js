029035bfc4873532d72a6ba9d04fdc0d1d7c4ea0
Luis Salcedo <luis.salcedo0906@gmail.com>
2022-09-13 15:39:33 -0500

    cors middleware

--- api/app.js
+++ api/app.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import cors from 'cors';
 import { PORT } from './config/config.js';
 import v1UserRouter from './v1/routes/userRoutes.js';
 import v1OperationRouter from './v1/routes/operationRoutes.js';
@@ -7,6 +8,7 @@ import v1SwaggerDocs from './v1/swagger.js';
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 app.use('/api/v1/users', v1UserRouter);
 app.use('/api/v1/operations', v1OperationRouter);
 
