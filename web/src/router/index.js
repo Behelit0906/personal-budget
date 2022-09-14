ff9db63931c6ddc06f267dffbd0b44d03e8193fc
Luis Salcedo <luis.salcedo0906@gmail.com>
2022-09-14 15:12:13 -0500

    add home route - vue client

--- web/src/router/index.js
+++ web/src/router/index.js
@@ -1,11 +1,13 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import { useAuthStore, useAlertStore } from '../stores';
 import accountRoutes from './account.routes';
+import Home from '../views/Home.vue';
 
 export const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   linkActiveClass: 'active',
   routes: [
+    { path: '/', component: Home },
     { ...accountRoutes },
     // catch all redirect to home page
     { path: '/:pathMatch(.*)*', redirect: '/account/login' },
