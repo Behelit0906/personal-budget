120f7d2c3492811e77f81da8e218df5549a07b0f
Luis Salcedo <luis.salcedo0906@gmail.com>
2022-09-14 09:57:23 -0500

    create handleResponse in fetchWrapper - vue client

--- web/src/helpers/fetchWrapper.js
+++ web/src/helpers/fetchWrapper.js
@@ -15,6 +15,26 @@ function request(method) {
   };
 }
 
+async function handleResponse(response) {
+  const isJson = response.headers?.get('content-type')?.includes('application/json');
+  const data = isJson ? await response.json() : null;
+
+  // check for error response
+  if (!response.ok) {
+    const { user, logout } = useAuthStore();
+    if ([401, 403].includes(response.status) && user) {
+      // auto logout if 401 Unauthorized or 403 Forbidden response returned from api
+      logout();
+    }
+
+    // get error message from body or default to response status
+    const error = (data && data.message) || response.status;
+    return Promise.reject(error);
+  }
+
+  return data;
+}
+
 export const fetchWrapper = {
   get: request('GET'),
   post: request('POST'),
