2c56a90d95ecc1d0afae693849100671ba2be3d4
Luis Salcedo <luis.salcedo0906@gmail.com>
2022-09-14 09:11:17 -0500

    create login method in auth store - vue client

--- web/src/stores/authStore.js
+++ web/src/stores/authStore.js
@@ -1,6 +1,7 @@
 import { defineStore } from "pinia";
 import { ref } from "vue";
 import { fetchWrapper } from "./helpers/fetchWrapper";
+import { useAlertStore } from "./stores";
 import { router } from "./router";
 
 const baseUrl = `${import.meta.env.VITE_API_URL}/users`;
@@ -8,4 +9,19 @@ const baseUrl = `${import.meta.env.VITE_API_URL}/users`;
 export const useAuthStore = defineStore("auth", () => {
   const user = ref("");
   const returnUrl = ref(null);
+
+  const login = async (email, password) => {
+    try {
+      user = await fetchWrapper.post(`${baseUrl}/login`, {
+        email,
+        password,
+      });
+
+      localStorage.setItem("user", JSON.stringify(user));
+      router.push(this.returnUrl || "/");
+    } catch (error) {
+      const alertStore = useAlertStore();
+      alertStore.error(error);
+    }
+  };
 });
