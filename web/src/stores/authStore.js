e7e16c3afc6decfd219add7b60ca211232347dba
Luis Salcedo <luis.salcedo0906@gmail.com>
2022-09-14 10:02:57 -0500

    format with prettier

--- web/src/stores/authStore.js
+++ web/src/stores/authStore.js
@@ -1,24 +1,24 @@
-import { defineStore } from "pinia";
-import { ref } from "vue";
-import { fetchWrapper } from "./helpers/fetchWrapper";
-import { useAlertStore } from "./stores";
-import { router } from "./router";
+import { defineStore } from 'pinia';
+import { ref } from 'vue';
+import { fetchWrapper } from './helpers/fetchWrapper';
+import { useAlertStore } from './stores';
+import { router } from './router';
 
 const baseUrl = `${import.meta.env.VITE_API_URL}/users`;
 
-export const useAuthStore = defineStore("auth", () => {
-  const user = ref("");
+export const useAuthStore = defineStore('auth', () => {
+  const user = ref('');
   const returnUrl = ref(null);
 
   const login = async (email, password) => {
     try {
-      user = await fetchWrapper.post(`${baseUrl}/login`, {
+      user.value = await fetchWrapper.post(`${baseUrl}/login`, {
         email,
         password,
       });
 
-      localStorage.setItem("user", JSON.stringify(user));
-      router.push(this.returnUrl || "/");
+      localStorage.setItem('user', JSON.stringify(user.value));
+      router.push(this.returnUrl || '/');
     } catch (error) {
       const alertStore = useAlertStore();
       alertStore.error(error);
@@ -26,9 +26,9 @@ export const useAuthStore = defineStore("auth", () => {
   };
 
   const logout = () => {
-    user = "";
-    localStorage.removeItem("user");
-    router.push("/account/login");
+    user = '';
+    localStorage.removeItem('user');
+    router.push('/account/login');
   };
 
   return { user, returnUrl, login, logout };
