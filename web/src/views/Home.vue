a7afa132a1e8d037a57b2b22f1b1f783da63f8de
Luis Salcedo <luis.salcedo0906@gmail.com>
2022-09-14 15:07:32 -0500

    create home view - vue client

---
+++ web/src/views/Home.vue
@@ -0,0 +1,16 @@
+<script setup>
+import { storeToRefs } from 'pinia';
+
+import { useAuthStore } from '../stores';
+
+const authStore = useAuthStore();
+const { user } = storeToRefs(authStore);
+</script>
+    
+<template>
+    <div v-if="user">
+        <h1>Hi {{user.name}}!</h1>
+
+    </div>
+</template>
+    
