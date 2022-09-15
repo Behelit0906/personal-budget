dc5ede38810f9d9f9ab9922047f5352b19a93f0c
Luis Salcedo <luis.salcedo0906@gmail.com>
2022-09-15 10:04:38 -0500

    change baseRoute in userStore - vue client

--- web/src/stores/userStore.js
+++ web/src/stores/userStore.js
@@ -3,12 +3,10 @@ import { ref } from 'vue';
 
 import { fetchWrapper } from '../helpers/fetchWrapper';
 
-const baseUrl = `${import.meta.env.VITE_API_URL}/users`;
+const baseUrl = import.meta.env.VITE_API_URL;
 
 export const useUserStore = defineStore('user', () => {
   const register = async userData => {
-    fetchWrapper.post(`${baseUrl}/register`, userData);
+    fetchWrapper.post(`${baseUrl}/users/register`, userData);
   };
-
-  return { register };
 });
