53eda0b1e9502ddf583ba277ef97ecb18c5a5cf1
Luis Salcedo <luis.salcedo0906@gmail.com>
2022-09-15 11:13:41 -0500

    create operationList view, bases - vue client

---
+++ web/src/views/operations/List.vue
@@ -0,0 +1,15 @@
+<script setup>
+import { storeToRefs } from 'pinia';
+import { ref, computed } from 'vue';
+
+import { useUserStore } from '../../stores';
+
+const page = ref(1);
+const limit = ref(5);
+const userStore = useUserStore();
+
+
+</script>
+    
+
+    
