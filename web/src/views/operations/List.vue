caabe2091247a9f1d3d73dfcae99394534e6b22a
Luis Salcedo <luis.salcedo0906@gmail.com>
2022-09-15 15:00:40 -0500

    replace computed prop by method, listView-vueClien

--- web/src/views/operations/List.vue
+++ web/src/views/operations/List.vue
@@ -1,24 +1,25 @@
 <script setup>
-import { storeToRefs } from 'pinia';
-import { ref, computed } from 'vue';
-
+import { ref, onMounted } from 'vue';
 import { useUserStore } from '../../stores';
 
 const page = ref(1);
 const limit = ref(5);
 const userStore = useUserStore();
+const operationsList = ref([]);
+const pages = ref(0);
 
-const operations = computed(async () => {
+async function getList() {
 	let temp = await userStore.getANumberOfOperations(page.value, limit.value);
-	temp = temp.data;
-	temp = temp.map(op => {
+	console.log(temp);
+	temp.data = temp.data.map(op => {
 		op.date = op.date.match(/[0-9]*-[0-9]*-[0-9]*/g);
 		return op;
 	});
-
-});
+	return temp;
+}
 
 </script>
+
     
 
     
