915474709cc59226c4853d931d8e85156855d160
Luis Salcedo <luis.salcedo0906@gmail.com>
2022-09-21 12:35:30 -0500

    replace bootstrap by vanilla css in register view

--- web/src/views/account/Register.vue
+++ web/src/views/account/Register.vue
@@ -12,7 +12,9 @@ const schema = Yup.object().shape({
     email: Yup.string().email().required('Email is required'),
     password: Yup.string()
         .required('Password is required')
-        .matches(passRegex, 'Password must be at least 8 characters, no more than 15 characters, and must include at least one upper case letter, one lower case letter, one numeric digit, and one special character.')
+        .matches(passRegex, 'Password must be at least 8 characters, no more than 15 characters, and must include at least one upper case letter, one lower case letter, one numeric digit, and one special character.'),
+    password_confirmation: Yup.string().required('Passwords must match')
+        .oneOf([Yup.ref('password'), null], 'Passwords must match')
 });
 
 async function onSubmit(values) {
@@ -29,35 +31,37 @@ async function onSubmit(values) {
 </script>
     
 <template>
-    <div class="card m-3">
-        <h4 class="card-header">Register</h4>
-        <div class="card-body">
-            <Form @submit="onSubmit" :validation-schema="schema" v-slot="{ errors, isSubmitting }">
-                <div class="form-group">
-                    <label>Name</label>
-                    <Field name="name" type="text" class="form-control" :class="{ 'is-invalid': errors.name }" />
-                    <div class="invalid-feedback">{{ errors.name }}</div>
-                </div>
-                <div class="form-group">
-                    <label>Email</label>
-                    <Field name="username" type="text" class="form-control" :class="{ 'is-invalid': errors.email }" />
-                    <div class="invalid-feedback">{{ errors.email }}</div>
-                </div>
-                <div class="form-group">
-                    <label>Password</label>
-                    <Field name="password" type="password" class="form-control"
-                        :class="{ 'is-invalid': errors.password }" />
-                    <div class="invalid-feedback">{{ errors.password }}</div>
-                </div>
-                <div class="form-group">
-                    <button class="btn btn-primary" :disabled="isSubmitting">
-                        <span v-show="isSubmitting" class="spinner-border spinner-border-sm mr-1"></span>
-                        Register
-                    </button>
-                    <router-link to="login" class="btn btn-link">Cancel</router-link>
-                </div>
-            </Form>
+    <h4 class="header">Register</h4>
+    <Form class="form" @submit="onSubmit" :validation-schema="schema" v-slot="{ errors }">
+        <div>
+            <p class="gray-font">Name</p>
+            <Field class="input" name="name" type="text" />
+            <div class="feedback">{{ errors.name }}</div>
         </div>
-    </div>
+        <div>
+            <p class="gray-font">Email</p>
+            <Field class="input" name="email" type="text" />
+            <div class="feedback">{{ errors.email }}</div>
+        </div>
+        <div>
+            <p class="gray-font">Password</p>
+            <Field class="input" name="password" type="password" />
+            <div class="feedback">{{ errors.password }}</div>
+        </div>
+        <div>
+            <p class="gray-font">Password</p>
+            <Field class="input" name="password_confirmation" type="password" />
+            <div class="feedback">{{ errors.password_confirmation }}</div>
+        </div>
+        <button class="button">Register</button>
+        <router-link to="login" class="link gray-font">Or Sign In</router-link>
+    </Form>
 </template>
+
+<style scoped>
+.form {
+    margin-left: 30px;
+    margin-right: 30px;
+}
+</style>
     
