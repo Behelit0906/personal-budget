974f4ecdec25eb5e0cd8f2c64f9649100f405a03
Luis Salcedo <luis.salcedo0906@gmail.com>
2022-09-21 11:35:57 -0500

    replace bootstrap by vanilla css in alertComponent

--- web/src/components/Alert.vue
+++ web/src/components/Alert.vue
@@ -7,12 +7,38 @@ const { alert } = storeToRefs(alertStore);
 </script>
 
 <template>
-    <div v-if="alert" class="container">
-        <div class="m-3">
-            <div class="alert alert-dismissable" :class="alert.type">
-                <button @click="alertStore.clear()" class="btn btn-link close">&times;</button>
-                {{alert.message}}
-            </div>
-        </div>
+    <div v-if="alert" class="alert-container" :class="alert.type">
+        {{alert.message}}
+        <div @click="alertStore.clear()" class="alert-button">&times;</div>
     </div>
 </template>
+
+<style scoped>
+.alert-container {
+    width: fit-content;
+    margin: 10px auto;
+    top: 20px;
+    padding: 10px 20px;
+    font-family: 'Roboto', sans-serif;
+    font-weight: 300;
+    text-align: center;
+    border-radius: 10px;
+}
+
+.alert-button {
+    display: inline-block;
+    margin-left: 10px;
+    font-size: 1.5rem;
+    cursor: pointer;
+}
+
+.alert-success {
+    background-color: #D1ECF1;
+    color: #329FD5;
+}
+
+.alert-danger {
+    background-color: #F8D7DA;
+    color: #AE3C24;
+}
+</style>
