4a8e4d3967f11a3375e0589b20f2a95372ba4bb1
Luis Salcedo <luis.salcedo0906@gmail.com>
2022-09-26 15:38:18 -0500

    add styles to operation layout

--- web/src/views/operations/Layout.vue
+++ web/src/views/operations/Layout.vue
@@ -1,7 +1,21 @@
 <template>
-    <div class="p-4">
-        <div class="container">
-            <router-view />
-        </div>
+    <div class="operations-container">
+        <router-view />
     </div>
 </template>
+
+<style>
+.operations-container {
+    width: 90%;
+    margin: 5% auto;
+    padding-top: 1%;
+    padding-bottom: 5%;
+    font-family: 'Roboto', sans-serif;
+    font-weight: 300;
+    border-radius: 5px;
+    background-color: #ffffff;
+}
+</style>
+
+
+
