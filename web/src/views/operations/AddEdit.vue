c49634a85bd7cd6cc64869a54f35a3ef694fb2ca
Luis Salcedo <luis.salcedo0906@gmail.com>
2022-09-28 10:08:00 -0500

    add base script in AddEdit operation view

--- web/src/views/operations/AddEdit.vue
+++ web/src/views/operations/AddEdit.vue
@@ -1,4 +1,34 @@
 <script setup>
+import { Form, Field } from 'vee-validate';
+import * as Yup from 'yup';
+import { useRoute } from 'vue-router';
+
+import { useUserStore, useAlertStore } from '../../stores';
+import { router } from '../../router';
+import { onMounted, ref } from 'vue';
+
+const userStore = useUserStore();
+const alertStore = useAlertStore();
+const route = useRoute();
+const id = route.params.id;
+const operation = ref(null);
+let title = 'Add operation';
+
+
+
+onMounted(async () => {
+    if (id) {
+        const temp = await userStore.getOperationById(id);
+        if (!temp.data.length)
+            await router.push('/operations');
+
+        else {
+            operation.value = temp.data;
+            title = 'Edit operation';
+        }
+    }
+});
+
 </script>
 
 <template>
