610eb733975a7999389d07c2916d58ff08ba119f
Luis Salcedo <luis.salcedo0906@gmail.com>
2022-09-28 08:59:31 -0500

    add edit and add routes in operation routes

--- web/src/router/operations.routes.js
+++ web/src/router/operations.routes.js
@@ -3,5 +3,9 @@ import { Layout, List } from '../views/operations';
 export default {
   path: '/operations',
   component: Layout,
-  children: [{ path: '', component: List }],
+  children: [
+    { path: '', component: List },
+    { path: 'add', component: AddEdit },
+    { path: 'edit/:id', component: AddEdit },
+  ],
 };
