0ac32c19a65c423f0e4fd10b4bf6572ad08a276c
Luis Salcedo <luis.salcedo0906@gmail.com>
2022-10-08 08:24:00 -0500

    add template to AddEdit component

--- web/src/views/operations/AddEdit.vue
+++ web/src/views/operations/AddEdit.vue
@@ -11,7 +11,7 @@ const userStore = useUserStore();
 const alertStore = useAlertStore();
 const route = useRoute();
 const id = route.params.id;
-const operation = ref(null);
+const operation = ref({});
 let title = computed(() => {
     return id ? 'Edit operation' : 'Add operation';
 });
@@ -23,14 +23,61 @@ onMounted(async () => {
         if (!temp.data.length)
             await router.push('/operations');
 
-        else
-            operation.value = temp.data;
+        else {
+            temp.data[0].date = temp.data[0].date.match(/[0-9]*-[0-9]*-[0-9]*/g);
+            operation.value = temp.data[0];
+        }
     }
 });
 
 </script>
 
 <template>
-    <h1 class="operations-header">{{title}}</h1>
+    <div class="operation-container">
+        <h1 class="header">{{title}}</h1>
+        <Form class="form">
+            <div>
+                <p class="gray-font">Name</p>
+                <Field class="input" name="name" type="text" v-model="operation.name" />
+                <div class="feedback gray-font"></div>
+            </div>
+            <div>
+                <p class="gray-font">Amount</p>
+                <Field class="input" name="amount" type="number" v-model="operation.amount" />
+                <div class="feedback gray-font"></div>
+            </div>
+            <div>
+                <p class="gray-font">Date</p>
+                <Field class="input" name="date" type="date" v-model="operation.date" />
+                <div class="feedback gray-font"></div>
+            </div>
+            <div>
+                <label class="gray-font">Type </label>
+                <Field name="type" as="select" class="select" v-model="operation.type">
+                    <option class="select" value="Income">Income</option>
+                    <option class="select" value="Egress">Egress</option>
+                </Field>
+                <div class="feedback gray-font"></div>
+            </div>
+            <button class="button">Save</button>
+        </Form>
+    </div>
 
 </template>
+
+<style scoped>
+.form {
+    margin-left: 30px;
+    margin-right: 30px;
+}
+
+.operation-container {
+    max-width: 400px;
+    margin-left: auto;
+    margin-right: auto;
+}
+
+.select {
+    font-weight: 300;
+}
+</style>
