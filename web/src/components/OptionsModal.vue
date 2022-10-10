0a7ba7f21a73f8251cd8f420b2a179d4fb3dbc8a
Luis Salcedo <luis.salcedo0906@gmail.com>
2022-10-10 10:11:21 -0500

    change modal's height

--- web/src/components/OptionsModal.vue
+++ web/src/components/OptionsModal.vue
@@ -19,12 +19,12 @@ function send(actionName) {
 .background {
     display: flex;
     justify-content: center;
-    align-items: center;
     position: absolute;
     top: 0;
     left: 0;
     width: 100%;
-    height: 100vh;
+    height: 120vh;
+    padding-top: 100%;
     background-color: rgba(0, 0, 0, 0.2);
 }
 
