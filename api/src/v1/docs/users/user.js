b94a4db2d4ef4ba2f4208d82c5e6e3b527b7ccdc
Luis Salcedo <luis.salcedo0906@gmail.com>
2022-10-10 16:00:53 -0500

    create swagger schema to user

---
+++ api/src/v1/docs/users/user.js
@@ -0,0 +1,22 @@
+/**
+ * @openapi
+ *  components:
+ *      schemas:
+ *          User:
+ *              type: object
+ *              required:
+ *                  - name
+ *                  - email
+ *                  - password
+ *              properties:
+ *                  name:
+ *                      type: string
+ *                  email:
+ *                      type: email
+ *                  password:
+ *                      type: string
+ *              example:
+ *                  name: Luis Salcedo
+ *                  email: luis@gmail.com
+ *                  password: 7DKGjhT58!
+ */
